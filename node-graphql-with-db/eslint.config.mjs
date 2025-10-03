fd8984a480d040dba1c118b4755f6a821406b780
Eamon Hyland <ehyland90@gmail.com>
2025-10-03 18:14:45 +1000

    wip

---
+++ node-graphql-with-db/eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default defineConfig(
+  { languageOptions: { globals: globals.node } },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+  {
+    files: ["src/graphql/generated.ts", "src/test/utils/client/generated.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+);
